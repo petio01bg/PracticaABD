4d5d3c8dbf626dc7ae27c81ea549d44e479bb486
Karlitosss <cargon26@ucm.es>
2022-04-05 09:33:44 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -3,5 +3,5 @@
     <a href=Resultados.php>Resultados</a>
     <a href=Goleadores.php>Goleadores</a>
 	<a href=Partidos.php>Partidos</a>
-	<a href=Asistentes.php>Asistentes</a>
+	<a href=Clubes.php>Clubes</a>
 </p>
