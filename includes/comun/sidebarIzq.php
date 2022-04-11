639c572571dcc90ba2c64f9e7a07e7c27afe5497
Karlitosss <cargon26@ucm.es>
2022-04-11 19:14:09 +0200

    submenu

--- includes/comun/sidebarIzq.php
+++ includes/comun/sidebarIzq.php
@@ -1,13 +1,16 @@
 <nav id="sidebarIzq">
-	<ul>
+	<ul class="menu">
 		<li><a href="index.php">Inicio</a></li>
 		<li><a href="goles.php">Goleadores</a></li>
 		<li><a href="asistencias.php">Asistentes</a></li>
 		<li><a href="salvadas.php">Salvadores</a></li>
 		<li><a href="">Operaciones</a>
-			<ul>
+			<ul class="submenu">
 			<li><a href="newJugador.php">Añadir jugador</a></li>
-			<li><a href="newJugador.php">Borrar jugador</a></li>
+			<li><a href="">Borrar jugador</a></li>
+			<li><a href="">Submenu 3</a></li>
+			<li><a href="">Submenu 4</a></li>
+			<li><a href="">Submenu 5</a></li>
 			</ul></li>
 	</ul>
 </nav>
